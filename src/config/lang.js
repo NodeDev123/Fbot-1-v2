19eb8464119d92c7b22bfed91d2ef8057d0bbe7d
NodeDev123 <nodedev237@gmail.com>
2024-11-29 23:27:41 +0100

    Update lang.js - code reviewed 2

--- src/config/lang.js
+++ src/config/lang.js
@@ -68,7 +68,7 @@ const lang = {
         share(ctx, user) {
             return `💥 Here is your referral link to send to your friends to earn money! ⚙️⬇️
 
-https://t.me/FreeMoneyAfffr_bot?start=user${ctx.from.id}
+https://t.me/FreMoneyAfffr_bot?start=user${ctx.from.id}
 
 🚀 Total number invited: ${user.invitedUsers} user(s) 💫
 oneyAf
@@ -133,7 +133,7 @@ oneyAf
         share(ctx, user) {
             return `💥 Voici ton lien de parrainage à envoyer à tes amis pour gagner de l’argent ! ⚙️ ⬇️
 
-https://t.me/FreeMoneyAfffr_bot?start=user${ctx.from.id}
+https://t.me/FreMoneyAfffr_bot?start=user${ctx.from.id}
 
 🚀 Nombre total invité : ${user.invitedUsers} utilisateur 💫 
 
