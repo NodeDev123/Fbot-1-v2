1aecc0b4cf9a7ab82de69848e59b49bf49d02413
LexTech <ryanvalentin288@gmail.com>
2024-11-29 22:35:54 +0100

    First commit - vode reveiwed

---
+++ src/utils/checkVerify.js
@@ -0,0 +1,39 @@
+// import channels from "../config/channels.json" assert { type: 'json' };
+
+import prisma from "../config/prisma.js";
+
+export async function accountValid(ctx) {
+    let CHANNELS = await prisma.channels.findMany({
+        where: {
+            type: "main",
+            joinRequest: false,
+            withdrawalChannel: false,
+            processStatus: {
+                notIn: ["0", "1", "2"],
+            },
+        },
+        select: {
+            tgID: true,
+        },
+    });
+
+    CHANNELS = [
+        ...CHANNELS,
+        {
+            tgID: "-1002279447886",
+        },
+    ];
+
+    const result = await CHANNELS.reduce(async (statPromise, channel) => {
+        const stat = await statPromise;
+        const user = await ctx.telegram.getChatMember(
+            channel.tgID,
+            ctx.from.id
+        );
+        const userLeft = !(user.status === "left" || user.status === "kicked");
+
+        return stat * userLeft;
+    }, true);
+
+    return result;
+}
